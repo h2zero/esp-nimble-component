dbc5899dbfc95761cc4685d6f8dc040304c3914e
Rahul Tank <rahul.tank@espressif.com>
2022-06-08 15:24:02 +0530

    Fix crash due to less co count

--- porting/npl/freertos/src/npl_os_freertos.c
+++ porting/npl/freertos/src/npl_os_freertos.c
@@ -98,8 +98,6 @@ static const char *TAG = "Timer";
 
 #define BLE_TOTAL_EVQ_COUNT (10)
 
-#define BLE_TOTAL_CO_COUNT (20)
-
 #define BLE_TOTAL_SEM_COUNT (10)
 
 #define BLE_TOTAL_MUTEX_COUNT (10)
@@ -135,7 +133,7 @@ static os_membuf_t ble_freertos_evq_buf[
 
 struct os_mempool ble_freertos_co_pool;
 static os_membuf_t ble_freertos_co_buf[
-    OS_MEMPOOL_SIZE(BLE_TOTAL_CO_COUNT, sizeof (struct ble_npl_callout_freertos))
+    OS_MEMPOOL_SIZE(MYNEWT_VAL(BLE_TOTAL_CO_COUNT), sizeof (struct ble_npl_callout_freertos))
 ];
 
 struct os_mempool ble_freertos_sem_pool;
@@ -1099,7 +1097,7 @@ int npl_freertos_mempool_init(void)
     if(!ble_freertos_evq_buf) {
         goto _error;
     }
-    ble_freertos_co_buf  = malloc(OS_MEMPOOL_SIZE(BLE_TOTAL_CO_COUNT, sizeof (struct ble_npl_callout_freertos)) * sizeof(os_membuf_t));
+    ble_freertos_co_buf  = malloc(OS_MEMPOOL_SIZE(MYNEWT_VAL(BLE_TOTAL_CO_COUNT), sizeof (struct ble_npl_callout_freertos)) * sizeof(os_membuf_t));
     if(!ble_freertos_co_buf) {
         goto _error;
     }
@@ -1128,7 +1126,7 @@ int npl_freertos_mempool_init(void)
         goto _error;
     }
 
-    rc = os_mempool_init(&ble_freertos_co_pool, BLE_TOTAL_CO_COUNT,
+    rc = os_mempool_init(&ble_freertos_co_pool, MYNEWT_VAL(BLE_TOTAL_CO_COUNT),
                          sizeof (struct ble_npl_callout_freertos), ble_freertos_co_buf,
                          "ble_freertos_co_pool");
     if(rc != 0) {
