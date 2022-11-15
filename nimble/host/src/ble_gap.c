1b37335371d5589d23223ed89f6f067959f2f8d5
timoxd7 <timoxd7@gmx.de>
2022-11-15 21:11:04 +0100

    Fixed min/max formatting

--- nimble/host/src/ble_gap.c
+++ nimble/host/src/ble_gap.c
@@ -29,11 +29,11 @@
 #include "ble_hs_resolv_priv.h"
 
 #ifndef min
-#define min(a, b) ((a)<(b)?(a):(b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
 #endif
 
 #ifndef max
-#define max(a, b) ((a)>(b)?(a):(b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
 #endif
 
 #if MYNEWT
