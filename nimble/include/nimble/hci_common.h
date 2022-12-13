2f93c28b47711d6a52de7e746ddedddc78f0bc9a
Rahul Tank <rahul.tank@espressif.com>
2022-12-13 11:25:13 +0530

    Add helper macros to set interval min / max for periodic adv

--- nimble/include/nimble/hci_common.h
+++ nimble/include/nimble/hci_common.h
@@ -1244,6 +1244,8 @@ struct ble_hci_le_set_transmit_power_report_enable_cp {
 #define BLE_HCI_ADV_ITVL_DEF                (0x800)         /* 1.28 seconds */
 #define BLE_HCI_ADV_CHANMASK_DEF            (0x7)           /* all channels */
 
+#define BLE_HCI_PERIODIC_ADV_ITVL           (1250)          /* usecs */
+
 /* Set scan parameters */
 #define BLE_HCI_SCAN_TYPE_PASSIVE           (0)
 #define BLE_HCI_SCAN_TYPE_ACTIVE            (1)
