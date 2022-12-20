452459458798d94b95f1193832bd6c6712d105e6
Rahul Tank <rahul.tank@espressif.com>
2022-12-20 14:07:53 +0530

    Added return value (success / failure ) to nimble_port_init

--- porting/examples/nuttx/main.c
+++ porting/examples/nuttx/main.c
@@ -72,7 +72,12 @@ int main(int argc, char *argv[])
     }
 
     printf("port init\n");
-    nimble_port_init();
+
+    ret = nimble_port_init();
+    if (ret != 0) {
+        printf(" Failed to init nimble %d \n", ret);
+	return ret;
+    }
 
     printf("hci init\n");
     ble_hci_sock_init();
