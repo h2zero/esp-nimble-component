mrsulaj-uvx/esp-nimble

Added checks to disable stack callback registered with controller during stack deinit

nimble/host/src/ble_hs.c
@@ -599,11 +599,12 @@ ble_hs_enqueue_hci_event(uint8_t *hci_evt)
     struct ble_npl_event *ev;
 
     ev = os_memblock_get(&ble_hs_hci_ev_pool);
-    if (ev == NULL) {
-        ble_hci_trans_buf_free(hci_evt);
-    } else {
+    if (ev && ble_hs_evq->eventq) {
         ble_npl_event_init(ev, ble_hs_event_rx_hci_ev, hci_evt);
         ble_npl_eventq_put(ble_hs_evq, ev);
+    } else {
+	/* Either ev is NULL or queue doesn't exist */
+        ble_hci_trans_buf_free(hci_evt);
     }
 }
 
@@ -856,6 +857,8 @@ ble_hs_deinit(void)
 #if BLE_MONITOR
     ble_monitor_deinit();
 #endif
+
+    ble_hci_trans_cfg_hs(NULL, NULL, NULL, NULL);
 
     ble_npl_mutex_deinit(&ble_hs_mutex);
 

h2zero/esp-nimble-component

nimble/host/src/ble_hs.c
@@ -581,11 +581,12 @@ ble_hs_enqueue_hci_event(uint8_t *hci_evt)
     struct ble_npl_event *ev;
 
     ev = os_memblock_get(&ble_hs_hci_ev_pool);
-    if (ev == NULL) {
-        ble_transport_free(hci_evt);
-    } else {
+    if (ev && ble_hs_evq->eventq) {
         ble_npl_event_init(ev, ble_hs_event_rx_hci_ev, hci_evt);
         ble_npl_eventq_put(ble_hs_evq, ev);
+    } else {
+	/* Either ev is NULL or queue doesn't exist */
+        ble_transport_free(hci_evt);
     }
 }
 
@@ -842,6 +843,8 @@ ble_hs_deinit(void)
 #if BLE_MONITOR
     ble_monitor_deinit();
 #endif
+
+    ble_hci_trans_cfg_hs(NULL, NULL, NULL, NULL);
 
     ble_npl_mutex_deinit(&ble_hs_mutex);
 