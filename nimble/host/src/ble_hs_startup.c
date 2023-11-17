1ab099b5896361aeccf1d8f1c2b8db0b1d8e0a67
Rahul Tank <rahul.tank@espressif.com>
2023-11-17 15:42:18 +0530

    fixed issue of incorrect IRK being set

--- nimble/host/src/ble_hs_startup.c
+++ nimble/host/src/ble_hs_startup.c
@@ -329,7 +329,6 @@ int
 ble_hs_startup_go(void)
 {
     int rc;
-    uint8_t irk[16];
 
     rc = ble_hs_startup_reset_tx();
     if (rc != 0) {
@@ -385,7 +384,7 @@ ble_hs_startup_go(void)
 
     ble_hs_pvcy_set_default_irk();
 
-    ble_hs_pvcy_set_our_irk(irk);
+    ble_hs_pvcy_set_our_irk(NULL);
 
     /* If flow control is enabled, configure the controller to use it. */
     ble_hs_flow_startup();
